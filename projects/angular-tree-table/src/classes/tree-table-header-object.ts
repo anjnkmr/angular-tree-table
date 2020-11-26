59c6b4e7390200756b8bd1c54789fbfa6447b4c7
Anjan Kumar <kumar.rakuditi@gmail.com>
2020-11-26 21:42:49 +0530

    added data types

--- projects/angular-tree-table/src/classes/tree-table-header-object.ts
+++ projects/angular-tree-table/src/classes/tree-table-header-object.ts
@@ -6,6 +6,7 @@ export class TreeTableHeaderObject {
     style: string;
     show: boolean;
     dataType: TtDataType = TtDataType.TEXT;
+    dataTypeArgs: {[key: string]: any} = {};
     enableColumnSearch = true; //It will take effect when column search is enabled in data
     canChangeVisbilityOnRuntime = true;
     constructor(title: string, dataProperty: string, style: string, show: boolean, canChangeVisibilityOnRuntime: boolean = true) {
--- src/app/components/inputs/inputs.component.ts
+++ src/app/components/inputs/inputs.component.ts
@@ -21,7 +21,7 @@ export class InputsComponent implements OnInit {
   populateDummyData() {
     const data = [];
     for (let i = 0; i < 120; i++) {
-      const row = new TreeTableRow(i + '', { sno: i+1, name: 'John '+(i+1), age: i+1}, false, null);
+      const row = new TreeTableRow(i + '', { sno: i+1, name: 'John '+(i+1), age: i+1, dob: '2020-09-23'}, false, null);
       row.options = [
         {
           displayText: 'Died',
@@ -47,8 +47,12 @@ export class InputsComponent implements OnInit {
     this.tableHeaders.push(new TreeTableHeaderObject('Sno', 'sno', null, true));
     this.tableHeaders.push(new TreeTableHeaderObject('Name', 'name', null, true));
     const ageHeader = new TreeTableHeaderObject('Age', 'age', null, true);
-    ageHeader.dataType = TtDataType.INPUT_TEXT;
+    ageHeader.dataType = TtDataType.INPUT_NUMBER;
+    ageHeader.dataTypeArgs = {'step': '0.001'};
     this.tableHeaders.push(ageHeader);
+    const dob = new TreeTableHeaderObject('DOB', 'dob', null, true);
+    dob.dataType = TtDataType.INPUT_DATE;
+    this.tableHeaders.push(dob);
     const diedHeader = new TreeTableHeaderObject('Died', 'died', null, true);
     diedHeader.dataType = TtDataType.INPUT_SELECT;
     this.tableHeaders.push(diedHeader);
